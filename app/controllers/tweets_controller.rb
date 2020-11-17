719183f042f8a3818cdaabd819d46628553fea58
shiseki toshikazu <uw-t.a.k.n.s-10ve@ezweb.ne.jp>
2020-11-17 13:24:33 +0000

    [Update] user, tweetコントローラーの更新

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,11 +1,16 @@
 class TweetsController < ApplicationController
+	before_action :authenticate_user!, except: [:index]
+
   def new
+  	@tweet = Tweet.new
   end
 
   def index
+  	@tweets = Tweet.all
   end
 
   def show
+  	@tweet = Tweet.find(params[:id])
   end
 
   def create
