8033e4be45b3cc199af4d5288e7042ef0f28720e
shiseki toshikazu <uw-t.a.k.n.s-10ve@ezweb.ne.jp>
2020-11-18 12:40:04 +0000

    [Update] tweetコントローラーの更新

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -14,5 +14,16 @@ class TweetsController < ApplicationController
   end
 
   def create
+    @tweet = Tweet.new(tweet_params)
+    @tweet.user_id = current_user.id
+    @tweet.save
+    redirect_to tweets_path
   end
+
+  private
+
+  def tweet_params
+    params.require(:tweet).permit(:body)
+  end
+
 end
