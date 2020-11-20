cea50761cbba20720f3cd20194ab6cc3eac03329
shiseki toshikazu <uw-t.a.k.n.s-10ve@ezweb.ne.jp>
2020-11-20 13:16:02 +0000

    [Update] userモデルの更新

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,14 @@ class User < ApplicationRecord
   has_many :tweets
   has_many :favorites
   has_many :favorite_tweets, through: :favorites, source: :tweet
+
+  has_many :passive_relationships, class_name: "Relationship", foreign_key: :follower_id
+  has_many :followings, through: :active_ralationships, source: :follower
+
+  has_many :passive_relationships, class_name: "Relationship", foreign_key: :follower_id
+  has_many :followers, through: :passive_relationships, source: :following
+
+  def followed_by?(user)
+  	passive_relationships.find_by(following_id: user.id).present?
+  end
 end
