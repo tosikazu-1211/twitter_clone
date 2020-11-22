2cc80a1618bf3475cd4a9ebd6126edb5b59f4514
shiseki toshikazu <uw-t.a.k.n.s-10ve@ezweb.ne.jp>
2020-11-22 13:29:30 +0000

    [Update] tweetコントローラの更新

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -12,6 +12,8 @@ class TweetsController < ApplicationController
   def show
   	@tweet = Tweet.find(params[:id])
     @user = @tweet.user
+    @comment = current_user.coments.build
+    @comments = @tweet.comments
   end
 
   def create
