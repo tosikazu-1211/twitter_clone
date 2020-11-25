ccd3b1d50f275f158dcbf83454160d77d65cc73f
shiseki toshikazu <uw-t.a.k.n.s-10ve@ezweb.ne.jp>
2020-11-25 08:23:24 +0000

    [Add] user検索機能の追加

--- config/routes.rb
+++ config/routes.rb
@@ -16,6 +16,7 @@ Rails.application.routes.draw do
   	resource :relationships, only: [:create, :destroy]
   	get :follows, on: :member
   	get :followers, on: :member
+    get :search, on: :collection
   end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
